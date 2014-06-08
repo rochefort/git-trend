389888369117ade8fb784453d4dd4e6a4be3b848
rochefort <terasawan@gmail.com>
2014-06-09 00:43:59 +0900

    Tweak rspec random testing

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -23,6 +23,7 @@ SimpleCov.start
 
 RSpec.configure do |config|
   config.expose_dsl_globally = false
+  config.order = :random
 # The settings below are suggested to provide a good initial experience
 # with RSpec, but feel free to customize to your heart's content.
 =begin
