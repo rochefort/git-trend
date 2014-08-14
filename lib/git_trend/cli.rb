0d3e6811e2908e1dcbd1eaca6938af274b68487c
rochefort <terasawan@gmail.com>
2014-08-15 06:40:34 +0900

    Tweak help descriptions

--- lib/git_trend/cli.rb
+++ lib/git_trend/cli.rb
@@ -15,10 +15,12 @@ module GitTrend
     end
 
     desc :list, "\033[32m(DEFAULT COMMAND)\e[0m List Trending repository on github [-l, -s, -d]"
-    option :language,    aliases: '-l', required: false
-    option :since,       aliases: '-s', required: false
-    option :description, aliases: '-d', required: false
+    option :language,    aliases: '-l', required: false, desc: 'Specify a language'
+    option :since,       aliases: '-s', required: false, desc: 'Enable: [daily, weekly, monthly]'
+    option :description, aliases: '-d', required: false, type: :boolean, desc: 'Dislpay descriptions'
+    option :help,        aliases: '-h', required: false, type: :boolean
     def list
+      help(:list) and return if  options[:help]
       scraper = Scraper.new
       projects = scraper.get(options[:language], options[:since])
       render(projects, !!options[:description])
