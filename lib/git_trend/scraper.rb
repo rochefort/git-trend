aab7b5b0b5a6fe5c62cbc717ad24dd945685bccc
rochefort <terasawan@gmail.com>
2016-01-29 02:03:59 +0900

    Fix missing languages
    It was caused by the github trending html was updated.

--- lib/git_trend/scraper.rb
+++ lib/git_trend/scraper.rb
@@ -24,11 +24,9 @@ module GitTrend
     def languages
       languages = []
       page = @agent.get(BASE_URL)
-      page.search('div.select-menu-item a').each do |content|
-        href = content.attributes['href'].value
-        # objective-c++ =>
-        language = href.match(%r{github.com/trending\?l=(.+)}).to_a[1]
-        languages << CGI.unescape(language) if language
+      page.search('.language-filter-list + .select-menu span.select-menu-item-text').each do |content|
+        language = content.text
+        languages << language if language
       end
       languages
     end
@@ -43,15 +41,23 @@ module GitTrend
       uri.to_s
     end
 
+    # Pattern 1: lang + stars
+    #  Ruby • 24 stars today • Built by
+    # Pattern 2: only stars
+    #  85 stars today • Built by
+    # Pattern 3: only lang
+    #  ASP • Built by
     def extract_lang_and_star_from_meta(text)
       meta_data = text.split('•').map { |x| x.delete("\n").strip }
-      if meta_data.size == 3
-        lang = meta_data[0]
-        star_count = meta_data[1].delete(',').to_i
-        [lang, star_count]
+      meta_data.pop # remove "Build by"
+      if meta_data.size == 2
+        [meta_data[0], meta_data[1].delete(',').to_i]
       else
-        star_count = meta_data[0].delete(',').to_i
-        ['', star_count]
+        if meta_data[0].include?('stars')
+          ['', meta_data[0].delete(',').to_i]
+        else
+          [meta_data[0], 0]
+        end
       end
     end
 
