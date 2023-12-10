ea0cc12a70f61b1c3dcb24ec6ecab8744c19f288
rochefort <terasawan@gmail.com>
2023-12-11 06:20:01 +0900

    Fix Style/SymbolProc

--- lib/git_trend/formatters/json_formatter.rb
+++ lib/git_trend/formatters/json_formatter.rb
@@ -3,7 +3,7 @@ require "json"
 module GitTrend::Formatters
   class JsonFormatter
     def print(projects, options)
-      puts projects.map { |project| project.to_h }.to_json
+      puts projects.map(&:to_h).to_json
     end
 
     def print_languages(languages)
