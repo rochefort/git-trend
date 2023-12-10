6a8c60def8c7e9be3781c6bb4dd5435d4e0f4d7c
rochefort <terasawan@gmail.com>
2023-12-11 07:29:24 +0900

    Fix RSpec::AnyInstance

--- spec/git_trend_spec.rb
+++ spec/git_trend_spec.rb
@@ -1,81 +1,95 @@
+include GitTrend
 RSpec.describe GitTrend do
+  let(:scraper_mock) { instance_double(Scraper) }
+
   before do
+    allow(Scraper).to receive(:new).and_return(scraper_mock)
     stub_request(:get, /.*/)
       .to_return(status: 200, headers: { content_type: "text/html" }, body: load_http_stub("trending"))
   end
 
   describe "#get" do
-    context "without options" do
-      it "Scraper#get call without options" do
-        expect_any_instance_of(Scraper).to receive(:get).with(no_args)
-        described_class.get
+    context "normal" do
+      before do
+        allow(scraper_mock).to receive(:get)
+      end
+      context "without options" do
+        it "Scraper#get call without options" do
+          described_class.get
+          expect(scraper_mock).to have_received(:get).with(no_args)
+        end
+      end
+
+      context "parameter is 'ruby'" do
+        it "Scraper#get call with 'ruby'" do
+          described_class.get("ruby")
+          expect(scraper_mock).to have_received(:get).with("ruby")
+        end
+      end
+
+      context "parameter is :ruby" do
+        it "Scraper#get call with :ruby" do
+          described_class.get(:ruby)
+          expect(scraper_mock).to have_received(:get).with(:ruby)
+        end
+      end
+
+      context "parameter is since: :weekly" do
+        it "Scraper#get call with [nil, :weekly]" do
+          described_class.get(since: :weekly)
+          expect(scraper_mock).to have_received(:get).with(nil, :weekly)
+        end
+      end
+
+      context "parameter is since: :week" do
+        it "Scraper#get call with [nil, :week]" do
+          described_class.get(since: :week)
+          expect(scraper_mock).to have_received(:get).with(nil, :week)
+        end
+      end
+
+      context "parameter is since: :w" do
+        it "Scraper#get call with [nil, :w]" do
+          described_class.get(since: :w)
+          expect(scraper_mock).to have_received(:get).with(nil, :w)
+        end
+      end
+
+      context "parameters are 'ruby', 'weekly'" do
+        it "Scraper#get call with ['ruby', 'weekly']" do
+          described_class.get("ruby", "weekly")
+          expect(scraper_mock).to have_received(:get).with("ruby", "weekly")
+        end
+      end
+
+      context "parameters are :ruby, :weekly" do
+        it "Scraper#get call with [:ruby, :weekly]" do
+          described_class.get(:ruby, :weekly)
+          expect(scraper_mock).to have_received(:get).with(:ruby, :weekly)
+        end
+      end
+
+      context "parameters are language: :ruby, since: :weekly" do
+        it "Scraper#get call with [:ruby, :weekly]" do
+          described_class.get(language: :ruby, since: :weekly)
+          expect(scraper_mock).to have_received(:get).with(:ruby, :weekly)
+        end
       end
     end
 
-    context "parameter is 'ruby'" do
-      it "Scraper#get call with 'ruby'" do
-        expect_any_instance_of(Scraper).to receive(:get).with("ruby")
-        described_class.get("ruby")
+    context "abnormal" do
+      context "when too many parameters" do
+        it { expect { described_class.get("ruby", "weekly", "many_params") }.to raise_error(Exception) }
       end
-    end
-
-    context "parameter is :ruby" do
-      it "Scraper#get call with :ruby" do
-        expect_any_instance_of(Scraper).to receive(:get).with(:ruby)
-        described_class.get(:ruby)
-      end
-    end
-
-    context "parameter is since: :weekly" do
-      it "Scraper#get call with [nil, :weekly]" do
-        expect_any_instance_of(Scraper).to receive(:get).with(nil, :weekly)
-        described_class.get(since: :weekly)
-      end
-    end
-
-    context "parameter is since: :week" do
-      it "Scraper#get call with [nil, :week]" do
-        expect_any_instance_of(Scraper).to receive(:get).with(nil, :week)
-        described_class.get(since: :week)
-      end
-    end
-
-    context "parameter is since: :w" do
-      it "Scraper#get call with [nil, :w]" do
-        expect_any_instance_of(Scraper).to receive(:get).with(nil, :w)
-        described_class.get(since: :w)
-      end
-    end
-
-    context "parameters are 'ruby', 'weekly'" do
-      it "Scraper#get call with ['ruby', 'weekly']" do
-        expect_any_instance_of(Scraper).to receive(:get).with("ruby", "weekly")
-        described_class.get("ruby", "weekly")
-      end
-    end
-
-    context "parameters are :ruby, :weekly" do
-      it "Scraper#get call with [:ruby, :weekly]" do
-        expect_any_instance_of(Scraper).to receive(:get).with(:ruby, :weekly)
-        described_class.get(:ruby, :weekly)
-      end
-    end
-
-    context "parameters are language: :ruby, since: :weekly" do
-      it "Scraper#get call with [:ruby, :weekly]" do
-        expect_any_instance_of(Scraper).to receive(:get).with(:ruby, :weekly)
-        described_class.get(language: :ruby, since: :weekly)
-      end
-    end
-
-    context "when too many parameters" do
-      it { expect { described_class.get("ruby", "weekly", "many_params") }.to raise_error(Exception) }
     end
 
     describe "#languages" do
+      before do
+        allow(scraper_mock).to receive(:languages)
+      end
       it "Scraper#languages call" do
-        expect_any_instance_of(Scraper).to receive(:languages).with(no_args)
         described_class.languages
+        expect(scraper_mock).to have_received(:languages).with(no_args)
       end
     end
   end
